0072ffb49c83a799e6e8ff28f04315f649bf5b57
shenando <shenandoah.grace@gmail.com>
2021-08-13 18:03:13 -0400

    finish pagination functionality

--- src/App.js
+++ src/App.js
@@ -3,30 +3,35 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'
 import Search from './components/users/Search'
 import axios from 'axios'
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Pagination from './components/users/Pagination';
 
 const App = () => {
   const [users, setUsers] = useState([])
   const [totalResults, setTotalResults] = useState(null)
   const [currentPage, setCurrentPage] = useState(1)
-  const [usersPerPage, setUsersPerPage] = useState(10)
+  const [usersPerPage] = useState(10)
 
-  useEffect(() => {
-
-  })
   // Search users
   const searchUsers = async text => {
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}`)
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&per_page=100&page=1`)
 
     setUsers(res.data.items)
     setTotalResults(res.data.total_count)
+
+    console.log(totalResults)
   }
 
   // Get first page of users
   const indexOfLastUser = currentPage * usersPerPage
   const indexOfFirstUser = indexOfLastUser - usersPerPage
   const currentUser = users.slice(indexOfFirstUser, indexOfLastUser)
+
+  // Click on to next page
+  const paginate = (pages) => {
+    setCurrentPage(pages)
+    console.log(pages)
+  }
 
   return (
     <div className="App">
@@ -35,11 +40,10 @@ const App = () => {
           <Search 
             searchUsers={searchUsers}/
           >
-          
+          <Pagination usersPerPage={usersPerPage} totalUsers={users.length} paginate={paginate}/>
           <Users users={currentUser} />
-          
       </div>
-      <Pagination usersPerPage={usersPerPage} totalResults={totalResults}/>
+
     </div>
   );
 }
--- src/components/users/Pagination.js
+++ src/components/users/Pagination.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Pagination = ({ usersPerPage, totalResults}) =>{
+const Pagination = ({ usersPerPage, totalUsers, paginate }) =>{
+  const [currentPage, setCurrentPage] = useState(0)
   const pages = []
 
-  for(let i = 1; i <= Math.ceil(totalResults/usersPerPage); i++) {
+  for(let i = 1; i <= Math.ceil(totalUsers/usersPerPage); i++) {
     pages.push(i)
   }
 
+  
+
   return (
     <div className="center-align">
-      <span>Your search returned {totalResults} results</span>
+      {totalUsers > 0 && (
+      <h6>Your search returned {totalUsers} result(s)</h6> )}
         <ul className="pagination">
+          {pages.includes(currentPage -1) && <li><a onClick={() => {
+            setCurrentPage(currentPage - 1)
+            paginate(currentPage - 1)
+          }} href="#!"><i className="material-icons">chevron_left</i></a></li>}
           {pages.map(num => (
             <li key={num} className="waves-effect">
-              <a href="!#">{num}</a>
+              <a onClick={() => paginate(num)} href="!#">{num}</a>
             </li>
           ))}
+          {pages.includes(currentPage + 1) && <li className="waves-effect"><a onClick={ () => {
+            setCurrentPage(currentPage + 1)
+            paginate(currentPage + 1)
+          }} href="#!"><i className="material-icons">chevron_right</i></a></li>}
         </ul>
+        
     </div>
   )
 }
